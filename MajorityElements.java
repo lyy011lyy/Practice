93d647d006b8b31b60c64268b985129780075e59
lyy011lyy <lyy011lyy@gmail.com>
2015-10-25 12:29:23 -0400

    20151025 Majority elements

---
+++ MajorityElements.java
@@ -0,0 +1,8 @@
+public class Solution {
+    public int majorityElement(int[] nums) {
+        if (nums.length == 1) return nums[0];
+        
+        Arrays.sort(nums);
+        return nums[(nums.length/2)];
+    }
+}
